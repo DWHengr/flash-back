9a8f5c82bff89867635c86542495fdb5920a40f4
henamdu <duweihengmr@163.com>
2022-12-07 10:55:09 +0800

    feat: add database

---
+++ src/config/database.ts
@@ -0,0 +1,12 @@
+import { join } from 'path';
+
+export default {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '123456',
+  database: 'flash',
+  entities: [join(__dirname, '../', '**/**.entity{.ts,.js}')],
+  synchronize: true,
+};
