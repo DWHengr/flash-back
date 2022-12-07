35f14f942448429b90a341f499169eeeb2439804
henamdu <duweihengmr@163.com>
2022-12-07 10:54:35 +0800

    feat: modify swagger

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const appOptions = { cors: true };
   const app = await NestFactory.create(AppModule, appOptions);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('v1');
 
   const options = new DocumentBuilder()
     .setTitle('Forest Back App')
     .setDescription('The Forest API description')
     .setVersion('1.0')
-    .setBasePath('api')
-    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api/v1/docs', app, document);
